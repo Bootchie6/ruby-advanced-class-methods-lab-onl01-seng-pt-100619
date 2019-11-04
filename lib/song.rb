4c7441e776c81bc1a7fa4ed2995e4560b7137813
Jonathan Jopio <jonathanjopio@gmail.com>
2019-11-04 03:54:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,8 +40,8 @@ class Song
     self.all.sort_by {|song| song.name}
   end
   
-  def self.new_from_filename
-    
+  def self.new_from_filename(filename)
+    info = filename.split ()
   end
 end
 
